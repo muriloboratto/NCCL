7d425ef1ff69348848d147ca5357bb694e2061be
Murilo Boratto <muriloboratto@gmail.com>
2021-07-18 18:08:10 -0300

    Update laplacian-2d-mpi.c
     2D Laplacian MPI

--- seismic_benchmarks/laplacian/2D/laplacian-2d-mpi.c
+++ seismic_benchmarks/laplacian/2D/laplacian-2d-mpi.c
@@ -103,35 +103,35 @@ int main(int argc, char *argv[]){
    sz =  (double*) calloc (m * n, sizeof(double));
 
    dx = 1.;
-	dz = 1.;
+   dz = 1.;
 
-	PARA_RANGE_1(1, n, nprocs, myid, jsta, jend, vector_return);
+   PARA_RANGE_1(1, n, nprocs, myid, jsta, jend, vector_return);
 
-	jsta = vector_return[0];
-	jend = vector_return[1];
+   jsta = vector_return[0];
+   jend = vector_return[1];
 
    jsta2 = jsta;
-	jend2 = jend;
+   jend2 = jend;
 
-	if(myid == 0)
-	   jsta2 = 2;
+  if(myid == 0)
+    jsta2 = 2;
 
    if(myid == (nprocs-1))
-	   jend2 = n - 1;
+     jend2 = n - 1;
 
-	inext = myid + 1;
-	iprev = myid - 1;
+   inext = myid + 1;
+   iprev = myid - 1;
 
-	if(myid == ( nprocs - 1 ))
-	   inext = MPI_PROC_NULL;
+   if(myid == ( nprocs - 1 ))
+     inext = MPI_PROC_NULL;
 
-	if(myid == 0)
-	   iprev = MPI_PROC_NULL;
+   if(myid == 0)
+     iprev = MPI_PROC_NULL;
 
    /*Population of the matrix*/
-	for (i = 0; i < m; i++)
-	   for(j = jsta-1; j < jend; j++)
-	      a[i + j* n] = (i + j + 2) * 1.;  /*stored dates in column (major column)*/
+   for(i = 0; i < m; i++)
+     for(j = jsta-1; j < jend; j++)
+	 a[i + j* n] = (i + j + 2) * 1.;  /*stored dates in column (major column)*/
  
    MPI_Barrier(MPI_COMM_WORLD);
 
@@ -158,33 +158,33 @@ int main(int argc, char *argv[]){
    }
 
    MPI_Isend(&a[0 + (jend - 1)   * n]  ,   m, MPI_REAL8, inext, 1, MPI_COMM_WORLD, &isend1);
-	MPI_Isend(&a[0 + (jsta - 1)   * n]  ,   m, MPI_REAL8, iprev, 1, MPI_COMM_WORLD, &isend2);
-	MPI_Irecv(&a[0 + (jsta - 2)   * n]  ,   m, MPI_REAL8, iprev, 1, MPI_COMM_WORLD, &irecv1);
-	MPI_Irecv(&a[0 + (jend)       * n]  ,   m, MPI_REAL8, inext, 1, MPI_COMM_WORLD, &irecv2);
+   MPI_Isend(&a[0 + (jsta - 1)   * n]  ,   m, MPI_REAL8, iprev, 1, MPI_COMM_WORLD, &isend2);
+   MPI_Irecv(&a[0 + (jsta - 2)   * n]  ,   m, MPI_REAL8, iprev, 1, MPI_COMM_WORLD, &irecv1);
+   MPI_Irecv(&a[0 + (jend)       * n]  ,   m, MPI_REAL8, inext, 1, MPI_COMM_WORLD, &irecv2);
 
-	MPI_Wait(&isend1, &status);
-	MPI_Wait(&isend2, &status);
-	MPI_Wait(&irecv1, &status);
-	MPI_Wait(&irecv2, &status);
+   MPI_Wait(&isend1, &status);
+   MPI_Wait(&isend2, &status);
+   MPI_Wait(&irecv1, &status);
+   MPI_Wait(&irecv2, &status);
 
-	for(j = jsta2-1; j < jend2; j++){
+   for(j = jsta2-1; j < jend2; j++){
       for(i = 1; i < (m - 1); i++){
 	     sx[i + j * n] = a[(i-1) + j*n] + a[(i+1)+ j*n] + 2 * a[i + j*n];
 	     sz[i + j * n] = a[ i + (j-1)*n] + a[i + (j+1)*n] + 2 * a[i + j*n];
 	      c[i + j * n] = (sx[i + j*n]/(dx*dx)) + (sz[i+j*n]/(dz*dz));
-	  }
-	}
+      }
+   }
 
-	MPI_Reduce(c, b, m * n, MPI_REAL8, MPI_SUM, 0, MPI_COMM_WORLD);
+   MPI_Reduce(c, b, m * n, MPI_REAL8, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	if (myid == 0){
+   if (myid == 0){
       show_matrix(b, n);
-	   //file = fopen("laplaciano_c.dat", "wb");
+      //file = fopen("laplaciano_c.dat", "wb");
       //fwrite(b, sizeof(double), n * m, file);
       //fclose(file);
    }
 
-	free(a);
+   free(a);
    free(b);
    free(c);
    free(sx);
